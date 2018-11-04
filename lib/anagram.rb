4fa1a1a1cf6b36c178d55e96e60960f94faae4b1
Stephen Olson <stephenolson@gmail.com>
2018-11-04 16:59:42 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,13 @@ class Anagram
     @words = words
   end
   
-  def match(find)
-    
+  def match(find_anagram_in_this_string) 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
+      end
+    end 
   end
   
   
