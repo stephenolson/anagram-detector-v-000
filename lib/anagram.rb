0b91790a1bcf19e3a87dd2d757e793a6c6476f12
Stephen Olson <stephenolson@gmail.com>
2018-11-04 17:03:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(find) 
+    puts "Must find #{self.word} inside the following string: #{find}"
+    find.find_all do |word| 
+      if word.split("").sort == self.word.split("").sort 
+        word 
+      end
+    end 
+  end
+  
+end 
+
+
+
